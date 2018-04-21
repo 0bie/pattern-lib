24ecc3d3a65f83f1b98b4a91e3c8c8bdcdaf84f6
0bie <obie.egwim.git@gmail.com>
2018-04-20 23:37:23 -0700

    Update webpack to v4
    Change css production config to object for better context

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -1,9 +1,9 @@
 const webpack = require('webpack');
-const {cssProdConfig} = require('./postcss.config.js');
-const ExtractTextWebpackPlugin = require('extract-text-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const UglifyJsWebpackPlugin = require('uglifyjs-webpack-plugin');
 const CompressionWebpackPlugin = require('compression-webpack-plugin');
-
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const {cssLoader, postcssLoader, scssLoader} = require('./postcss.config.js').cssProdConfig;
 
 module.exports = {
   output: {
@@ -14,15 +14,45 @@ module.exports = {
     rules: [
       {
         test: /\.s?css$/,
-        use: ExtractTextWebpackPlugin.extract(cssProdConfig)
+        use: [
+          MiniCssExtractPlugin.loader,
+          cssLoader,
+          postcssLoader,
+          scssLoader
+        ]
       }
     ]
   },
+  optimization: {
+    nodeEnv: 'production',
+    usedExports: true,
+    occurrenceOrder: true,
+    minimizer: [
+      new UglifyJsWebpackPlugin({
+        cache: true,
+        parallel: true,
+        sourceMap: true
+      }),
+      new OptimizeCssAssetsPlugin({})
+    ],
+    splitChunks: {
+      cacheGroups: {
+        commons: {
+          chunks: 'initial',
+          minChunks: 3,
+          name: 'commons',
+          enforce: true
+        }
+      }
+    }
+  },
   plugins: [
     new webpack.HashedModuleIdsPlugin(),
-    new ExtractTextWebpackPlugin('styles/index.[chunkhash].css'),
     new UglifyJsWebpackPlugin({sourceMap: true}),
-    new webpack.optimize.CommonsChunkPlugin({name: 'manifest'}),
+    new MiniCssExtractPlugin({
+      filename: 'styles/[name].[chunkHash].css',
+      chunkFilename: 'styles/[id].css'
+    }),
     new CompressionWebpackPlugin({
       asset: '[path].gz[query]',
       algorithm: 'gzip',
@@ -32,7 +62,6 @@ module.exports = {
     }),
     new webpack.DefinePlugin({
       'process.env.NODE_ENV': 'production'
-    }),
-    new webpack.optimize.ModuleConcatenationPlugin()
+    })
   ]
 };
